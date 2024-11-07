ac555efd6fc6b8a68e21489cc881bb7f5af1d5d6
kevin7um <kevinpestudos@gmail.com>
2024-11-06 22:29:14 -0300

    Configuração da API IP Adress e correção de código

--- src/js/scripts/script.js
+++ src/js/scripts/script.js
@@ -1,9 +1,74 @@
+var apiKey = API_KEY;
+var ipDom = document.getElementById("ip");
+var localDom = document.getElementById("local");
+var timezoneDom = document.getElementById("timezone");
+var ispDom = document.getElementById("isp");
 
-var map = L.map('map').setView([-22.3311744,-41.7541465,], 13) ;
+var form = document.getElementById('js-form');
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+async function getLocation(input) {
+  try {
+    const response = await fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&domain=${input}`
+		);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+    console.log("Erro ao obter dados:", error);
+	}
+}
+
+
+function addDataToHTML(ip, city, region, timezone, isp) {
+  ipDom.innerText = ip;
+	localDom.innerText = city + ", " + region;
+	timezoneDom.innerText = timezone;
+	ispDom.innerText = isp;
+}
+
+// Leaflet
+var map = L.map("map", {
+  zoomControl: false,
+}).setView([23.719189, -29.3808503],3);
+
+//Exibindo o mapa
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  attribution:
+  '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-var marker = L.marker([-22.3311744,-41.7541465]).addTo(map)    
+var marker = L.marker([37.40599, -122.078514]).addTo(map);
+
+function addMapToHTML(lat, lng){
+  map.setView([lat, lng], 13);
+
+  // Adicionando marcador
+  marker.setLatLng([lat, lng]) 
+}    
+
+async function viewResult(search){
+  const data = await getLocation(search);
+  
+  addDataToHTML(
+    data.ip,
+    data.location.city,
+    data.location.region,
+    data.location.timezone,
+    data.isp
+  )
+  
+  addMapToHTML(
+    data.location.lat, 
+    data.location.lng
+  ) 
+  
+}
+
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+  
+  const search = document.getElementById('input').value
+  
+  viewResult(search) 
+})  
